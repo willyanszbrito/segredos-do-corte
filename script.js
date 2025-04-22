9990a4f0abb981208750876391bcba09cc68be9d
willyanszbrito <contatowillyanbrito@hotmail.com>
2025-04-21 21:15:28 -0400

    Adicionando as novas páginas e funcionalidades

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+// Redirecionar para a página de agendamento quando o botão "Agendar Agora" for clicado
 document.getElementById('agendarBtn').addEventListener('click', function() {
-    window.location.href = 'agendamento.html';  // Redireciona para a página de agendamento
-  });
-  
+  window.location.href = 'agendamento.html';  // Redireciona para a página de agendamento
+});
+
+// Seleção do dia da semana
+function selectDay(day) {
+  alert('Você selecionou ' + day + '. Agora escolha a data e horário para o agendamento.');
+}
